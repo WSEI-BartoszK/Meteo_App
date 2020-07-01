51c4756c18da85b74c90b7f6d75536b62e1cb9d7
WSEI-BartoszK <bartosz.kapusta@microsoft.wsei.edu.pl>
2020-07-01 14:43:02 +0200

    v0.2.1
    Fixed Menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,84 @@
-﻿using System;
+using System;
 
-namespace Meteo_App
+namespace Aplikacja_Pogodowa
 {
     class Program
     {
+        #region menu
+        static void MenuItems()
+        {
+            Spacer();
+            Console.WriteLine("Wybierz pozycję z menu [1-3]: ");
+            Console.WriteLine(" ");
+            Console.WriteLine("1: Sprawdź pogodę (https://danepubliczne.imgw.pl/)");
+            Console.WriteLine("2: O autorze");
+            Console.WriteLine("3: Zakończ Aplikacje");
+            Spacer();
+        }
+        static void Spacer()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" ");
+        }
+
+        static void CreatedBy()
+        {
+            Spacer();
+            Console.WriteLine("Bartosz Kapusta");
+            Console.WriteLine("Grupa lab8/1/IEN");
+            Console.WriteLine("Index 12494");
+            Spacer();
+        }
+        static void Anykey()
+        {
+            Console.Write("Naciśni dowolny klawisz.");
+            Console.ReadKey();
+            Spacer();
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region user input
+            int userInput;
+            string userChoice;
+            string city_name;
+            #endregion
+
+            do
+            {
+                MenuItems();
+
+                Console.Write("Wybierz pozycję z menu: ");
+                userChoice = Console.ReadLine();
+
+                // check if is number
+                if (!Int32.TryParse(userChoice, out userInput)) continue;
+
+                if (userChoice == "3")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (userChoice == "2")
+                {
+                    CreatedBy();
+                }
+
+                if (userChoice == "1")
+                {
+                    Console.Write("Enter city name: ");
+                    city_name = Console.ReadLine();
+
+                    Console.WriteLine("To be added");
+
+                    Spacer();
+
+
+                }
+                Anykey();
+            }
+            while (true);
         }
     }
 }
