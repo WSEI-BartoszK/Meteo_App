1cb2e7ca1258fc7fae731886122a77bcfd3bbfe9
WSEI-BartoszK <bartosz.kapusta@microsoft.wsei.edu.pl>
2020-07-02 22:32:55 +0200

    v0.4.1
    Adding forgot brackets []

---
+++ WeatherForecast.cs
@@ -0,0 +1,29 @@
+using System.Text.Json.Serialization;
+using System.Text.Json;public class WeatherForecast
+{
+    public int IsSuccess {get; set;}
+    public string ErrorMessage {get; set;}
+
+    public string RequestUrl {get; set;}
+    
+    JsonPropertyName("id_stacji")
+    public string WSID {get; set;}
+    JsonPropertyName("stacja")
+    public string CityName {get; set;}
+    JsonPropertyName("data_pomiaru")
+    public string Date {get; set;}
+    JsonPropertyName("godzina_pomiaru")
+    public string Hour {get; set;}
+    JsonPropertyName("temperatura")
+    public string Temperature {get; set;}
+    JsonPropertyName("predkosc_wiatru")
+    public string WindSpeed {get; set;}
+    JsonPropertyName("kierunek_wiatru")
+    public string WindDir{get; set;}
+    JsonPropertyName("wilgotnosc_wzgledna")
+    public string Humidity {get; set;}
+    JsonPropertyName("suma_opadu")
+    public string Rainfall {get; set;}
+    JsonPropertyName("cisnienie")
+    public string Pressure {get; set;}
+}
